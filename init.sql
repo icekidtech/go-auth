ff2184a14ca07a9f32c41e1367e5213f72c78a48
icekidtech <edwinidopise@gmail.com>
2026-05-17 16:48:53 +0100

    feat: add initial database creation script and .gitignore file

---
+++ init.sql
@@ -0,0 +1,8 @@
+-- Create database
+CREATE DATABASE go_auth_db;
+
+-- Connect to the database
+\c go_auth_db;
+
+-- The tables will be created automatically by Go migrations when you run the app
+-- This script just creates the database and ensures it exists
